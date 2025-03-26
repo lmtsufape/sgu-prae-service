d8ebda3a51af37427ee4f992d43403e4eabec059
Douglas Filipe <batista.douglasfs@gmail.com>
2025-03-26 17:42:42 -0300

    fix: atualização da fachada após a adição das entidades Tipo Bolsa, Tipo Auxílio e Auxílio

--- src/main/java/br/edu/ufape/sguPraeService/fachada/Fachada.java
+++ src/main/java/br/edu/ufape/sguPraeService/fachada/Fachada.java
@@ -1,9 +1,18 @@
 package br.edu.ufape.sguPraeService.fachada;
 
 
+import br.edu.ufape.sguPraeService.exceptions.AuxilioNotFoundException;
 import br.edu.ufape.sguPraeService.exceptions.ProfissionalNotFoundException;
+import br.edu.ufape.sguPraeService.exceptions.TipoAuxilioNotFoundException;
+import br.edu.ufape.sguPraeService.exceptions.TipoBolsaNotFoundException;
+import br.edu.ufape.sguPraeService.models.Auxilio;
 import br.edu.ufape.sguPraeService.models.Profissional;
+import br.edu.ufape.sguPraeService.models.TipoAuxilio;
+import br.edu.ufape.sguPraeService.models.TipoBolsa;
+import br.edu.ufape.sguPraeService.servicos.interfaces.AuxilioService;
 import br.edu.ufape.sguPraeService.servicos.interfaces.ProfissionalService;
+import br.edu.ufape.sguPraeService.servicos.interfaces.TipoAuxilioService;
+import br.edu.ufape.sguPraeService.servicos.interfaces.TipoBolsaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +21,9 @@ import java.util.List;
 @Component @RequiredArgsConstructor
 public class Fachada {
     private final ProfissionalService profissionalService;
+    private final TipoBolsaService tipoBolsaService;
+    private final TipoAuxilioService tipoAuxilioService;
+    private final AuxilioService auxilioService;
 
     // ------------------- Profissional ------------------- //
     public List<Profissional> listarProfissionais() {
@@ -32,5 +44,68 @@ public class Fachada {
 
     public void deletarProfissional(Long id) throws ProfissionalNotFoundException {
         profissionalService.deletar(id);
+    }
+
+    // ------------------- TipoBolsa ------------------- //
+    public List<TipoBolsa> listarTipoBolsas() {
+        return tipoBolsaService.listar();
+    }
+
+    public TipoBolsa buscarTipoBolsa(Long id) throws TipoBolsaNotFoundException {
+        return tipoBolsaService.buscar(id);
+    }
+
+    public TipoBolsa salvarTipoBolsa(TipoBolsa tipoBolsa) {
+        return tipoBolsaService.salvar(tipoBolsa);
+    }
+
+    public TipoBolsa editarTipoBolsa(Long id, TipoBolsa tipoBolsa) throws TipoBolsaNotFoundException {
+        return tipoBolsaService.editar(id, tipoBolsa);
+    }
+
+    public void deletarTipoBolsa(Long id) throws TipoBolsaNotFoundException {
+        tipoBolsaService.deletar(id);
+    }
+
+    // ------------------- TipoAuxilio ------------------- //
+    public List<TipoAuxilio> listarTipoAuxilios() {
+        return tipoAuxilioService.listar();
+    }
+
+    public TipoAuxilio buscarTipoAuxilio(Long id) throws TipoAuxilioNotFoundException {
+        return tipoAuxilioService.buscar(id);
+    }
+
+    public TipoAuxilio salvarTipoAuxilio(TipoAuxilio tipoAuxilio) {
+        return tipoAuxilioService.salvar(tipoAuxilio);
+    }
+
+    public TipoAuxilio editarTipoAuxilio(Long id, TipoAuxilio tipoAuxilio) throws TipoAuxilioNotFoundException {
+        return tipoAuxilioService.editar(id, tipoAuxilio);
+    }
+
+    public void deletarTipoAuxilio(Long id) throws TipoAuxilioNotFoundException {
+        tipoAuxilioService.deletar(id);
+    }
+
+    // ------------------- Auxilio ------------------- //
+    public List<Auxilio> listarAuxilios() {
+        return auxilioService.listar();
+    }
+
+    public Auxilio buscarAuxilio(Long id) throws AuxilioNotFoundException {
+        return auxilioService.buscar(id);
+    }
+
+    public Auxilio salvarAuxilio(Auxilio auxilio) {
+        return auxilioService.salvar(auxilio);
+    }
+
+    public Auxilio editarAuxilio(Long id, Auxilio auxilio) throws AuxilioNotFoundException {
+        return auxilioService.editar(id, auxilio);
+    }
+
+    public void deletarAuxilio(Long id) throws AuxilioNotFoundException {
+        auxilioService.deletar(id);
     }
 }
