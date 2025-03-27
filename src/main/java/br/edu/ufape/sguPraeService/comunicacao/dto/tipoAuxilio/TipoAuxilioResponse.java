83e8e993a8a458072c0f3a43c3f8588416c74543
Douglas Filipe <batista.douglasfs@gmail.com>
2025-03-27 17:25:38 -0300

    feat: Request, Response e Controller da entidade Tipo Auxílio

---
+++ src/main/java/br/edu/ufape/sguPraeService/comunicacao/dto/tipoAuxilio/TipoAuxilioResponse.java
@@ -0,0 +1,19 @@
+package br.edu.ufape.sguPraeService.comunicacao.dto.tipoAuxilio;
+import br.edu.ufape.sguPraeService.models.TipoAuxilio;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+import java.math.BigDecimal;
+
+@Getter @Setter
+public class TipoAuxilioResponse {
+
+    Long id;
+    String tipo;
+    BigDecimal valorAuxilio;
+
+    public TipoAuxilioResponse(TipoAuxilio tipoAuxilio, ModelMapper modelMapper){
+        if (tipoAuxilio == null) throw new IllegalArgumentException("Tipo Auxílio não pode ser nulo");
+        else modelMapper.map(tipoAuxilio, this);
+    }
+}
