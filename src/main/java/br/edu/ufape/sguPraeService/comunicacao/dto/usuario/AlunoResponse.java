feef204233148e5c5925d0315d2067a591e736c3
Caio <caio.vsg@hotmail.com>
2025-04-15 19:28:32 -0300

    feat: adição de id do usuário no response do aluno e do funcionario

--- src/main/java/br/edu/ufape/sguPraeService/comunicacao/dto/usuario/AlunoResponse.java
+++ src/main/java/br/edu/ufape/sguPraeService/comunicacao/dto/usuario/AlunoResponse.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AlunoResponse {
+    UUID id;
     String nome;
     String nomeSocial;
     String cpf;
--- src/main/java/br/edu/ufape/sguPraeService/comunicacao/dto/usuario/FuncionarioResponse.java
+++ src/main/java/br/edu/ufape/sguPraeService/comunicacao/dto/usuario/FuncionarioResponse.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class FuncionarioResponse {
+    UUID id;
     String nome;
     String nomeSocial;
     String cpf;
