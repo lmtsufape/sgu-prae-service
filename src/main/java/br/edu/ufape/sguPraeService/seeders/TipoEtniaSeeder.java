2c8942e73e1e02898d7d22902911a09e049c3b0b
NicolasGomes99 <nicolasgabrielgomes9@gmail.com>
2025-05-09 21:09:37 -0300

    fix: correção em criarDadosBancarios + seeder de TipoEtnia

---
+++ src/main/java/br/edu/ufape/sguPraeService/seeders/TipoEtniaSeeder.java
@@ -0,0 +1,35 @@
+package br.edu.ufape.sguPraeService.seeders;
+
+import br.edu.ufape.sguPraeService.models.TipoEtnia;
+import br.edu.ufape.sguPraeService.dados.TipoEtniaRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TipoEtniaSeeder {
+
+    private final TipoEtniaRepository tipoEtniaRepository;
+
+    public TipoEtniaSeeder(TipoEtniaRepository tipoEtniaRepository) {
+        this.tipoEtniaRepository = tipoEtniaRepository;
+    }
+
+    @EventListener
+    public void seed(ApplicationReadyEvent event) {
+        List<String> etnias = List.of("Branco", "Preto", "Pardo", "Indígena", "Amarelo");
+
+        for (String etnia : etnias) {
+            tipoEtniaRepository.findByTipo(etnia)
+                    .orElseGet(() -> {
+                        TipoEtnia novoTipo = new TipoEtnia();
+                        novoTipo.setTipo(etnia);
+                        return tipoEtniaRepository.save(novoTipo);
+                    });
+        }
+
+        System.out.println("Tipos de Etnia inicializados com sucesso!");
+    }
+}
