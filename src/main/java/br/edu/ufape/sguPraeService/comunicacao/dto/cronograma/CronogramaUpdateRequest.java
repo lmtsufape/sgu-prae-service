977abe76908ae57fee52f148e2772a12eed8e647
Caio <caio.vsg@hotmail.com>
2025-05-28 19:20:16 -0300

    feat: adicionar cronogramas em lote

--- src/main/java/br/edu/ufape/sguPraeService/comunicacao/dto/cronograma/CronogramaUpdateRequest.java
+++ src/main/java/br/edu/ufape/sguPraeService/comunicacao/dto/cronograma/CronogramaUpdateRequest.java
@@ -3,7 +3,6 @@ package br.edu.ufape.sguPraeService.comunicacao.dto.cronograma;
 import br.edu.ufape.sguPraeService.models.Cronograma;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,29 +12,20 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
-public class CronogramaRequest {
+public class CronogramaUpdateRequest {
 
     @JsonFormat(pattern = "dd/MM/yyyy")
-    @NotEmpty(message = "É origatória ao menos uma data")
-    private List<LocalDate> datas;
+    @NotNull(message = "Data é obrigatória")
+    private LocalDate data;
 
     @NotNull(message = "Tipo de atendimento é obrigatório")
     private Long tipoAtendimentoId;
 
-    public List<Cronograma> convertToEntities(ModelMapper modelMapper) {
+    public Cronograma convertToEntity(CronogramaUpdateRequest cronogramaRequest, ModelMapper modelMapper) {
         modelMapper.getConfiguration()
                 .setMatchingStrategy(MatchingStrategies.STRICT);
-
-        return datas.stream()
-                .map(data -> {
-                    Cronograma cronograma = modelMapper.map(this, Cronograma.class);
-                    cronograma.setData(data);
-                    return cronograma;
-                })
-                .collect(Collectors.toList());
+        return modelMapper.map(cronogramaRequest, Cronograma.class);
     }
 }
