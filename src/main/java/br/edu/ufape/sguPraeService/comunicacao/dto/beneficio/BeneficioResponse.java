fbbfb48a3847ff1766a018ce32303976edb06524
DavyEugenio <davy.eugenio@hotmail.com>
2025-07-02 16:33:22 -0300

    fix: correcao de tipo de estudante me DTO de Beneficio

--- src/main/java/br/edu/ufape/sguPraeService/comunicacao/dto/beneficio/BeneficioResponse.java
+++ src/main/java/br/edu/ufape/sguPraeService/comunicacao/dto/beneficio/BeneficioResponse.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.modelmapper.ModelMapper;
 
+import br.edu.ufape.sguPraeService.comunicacao.dto.estudante.EstudanteResponse;
 import br.edu.ufape.sguPraeService.comunicacao.dto.tipoBeneficio.TipoBeneficioResponse;
 import br.edu.ufape.sguPraeService.models.Beneficio;
 import br.edu.ufape.sguPraeService.models.Estudante;
@@ -28,7 +29,7 @@ public class BeneficioResponse {
 	private String parecerTermino;
 	private BigDecimal valorPagamento;
 	private boolean status;
-	private Estudante estudantes;
+	private EstudanteResponse estudantes;
 
 	public BeneficioResponse(Beneficio beneficio, ModelMapper modelMapper) {
 		if (beneficio == null)
