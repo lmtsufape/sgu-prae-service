2c765aa76aace204a6a9a8a97a412aa8c9ca8324
Vitorass0 <vitor.ass@hotmail.com>
2025-07-31 00:40:26 -0300

    Fix: Removendo tipo etnia deste módulo.

--- src/main/java/br/edu/ufape/sguPraeService/exceptions/TipoEtniaDuplicadoException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ufape.sguPraeService.exceptions;
-
-
-public class TipoEtniaDuplicadoException extends RuntimeException {
-    public TipoEtniaDuplicadoException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/edu/ufape/sguPraeService/exceptions/notFoundExceptions/TipoEtniaNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ufape.sguPraeService.exceptions.notFoundExceptions;
-
-
-public class TipoEtniaNotFoundException extends NotFoundException {
-    public TipoEtniaNotFoundException() {
-        super("Tipo de etnia não encontrado");
-    }
-}
