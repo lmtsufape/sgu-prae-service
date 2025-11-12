8bfddd34351ac6797dd95f87788d43da7ad027a0
NicolasGomes99 <nicolasgabrielgomes9@gmail.com>
2025-11-12 16:21:41 -0300

    aumentando a cobertura de filtros

--- src/main/java/br/edu/ufape/sguPraeService/dados/AgendamentoRepository.java
+++ src/main/java/br/edu/ufape/sguPraeService/dados/AgendamentoRepository.java
@@ -5,12 +5,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
+import br.edu.ufape.sguPraeService.models.QAgendamento;
+import com.querydsl.core.types.dsl.StringPath;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.lang.NonNull;
 
 
-import java.util.UUID;
+import java.time.LocalDate;
+import java.util.*;
 
-public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long>,
+        QuerydslPredicateExecutor<Agendamento>,
+        QuerydslBinderCustomizer<QAgendamento> {
 
 
     Page<Agendamento> findAllByEstudante_UserIdAndAtivoTrue(UUID userId, Pageable pageable);
@@ -22,5 +31,34 @@ public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> 
             "WHERE p.userId = :userId and  a.ativo = true")
     Page<Agendamento> findAllByProfissionalUserId(@Param("userId") UUID userId, Pageable pageable);
 
+    @Override
+    default void customize(QuerydslBindings bindings, @NonNull QAgendamento root) {
+        // Habilita "contains" para todas as strings
+        bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
 
+        // Excluir atributos que não fazem sentido filtrar
+        bindings.excluding(root.ativo);
+
+        // --- Binding para data (suporta data exata e periodo) ---
+        bindings.bind(root.data).all((path, value) -> {
+            if (value.isEmpty()) {
+                return Optional.empty();
+            }
+            // Converte a coleção para uma lista
+            List<LocalDate> datas = new ArrayList<>(value);
+            Collections.sort(datas);
+            if (datas.size() == 1) {
+                // 1. Filtro de data exata
+                return Optional.of(path.eq(datas.getFirst()));
+            } else {
+                // 2. Filtro de período
+                return Optional.of(path.between(datas.getFirst(), datas.getLast()));
+            }
+        });
+
+        // Permitir filtragem por IDs de associações
+        bindings.bind(root.vaga.id).first((path, value) -> path.eq(value));
+        bindings.bind(root.estudante.id).first((path, value) -> path.eq(value));
+        bindings.bind(root.vaga.cronograma.id).first((path, value) -> path.eq(value));
+    }
 }
