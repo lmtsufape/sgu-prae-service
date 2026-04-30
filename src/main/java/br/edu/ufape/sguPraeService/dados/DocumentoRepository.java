7aea81cf2ca9b73fe00c26dd5457701631c86824
NicolasGomes99 <nicolasgabrielgomes9@gmail.com>
2026-04-30 19:44:12 -0300

    schedule pra limpar o disco removendo documentos orfãos

--- src/main/java/br/edu/ufape/sguPraeService/dados/DocumentoRepository.java
+++ src/main/java/br/edu/ufape/sguPraeService/dados/DocumentoRepository.java
@@ -2,6 +2,8 @@ package br.edu.ufape.sguPraeService.dados;
 
 import br.edu.ufape.sguPraeService.models.Documento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,12 @@ import java.util.UUID;
 
 @Repository
 public interface DocumentoRepository extends JpaRepository<Documento, Long> {
+
     List<Documento> findByUserId(UUID userId);
     Optional<Documento> findByIdAndUserId(Long id, UUID userId);
+
+    @Query("SELECT CASE WHEN COUNT(d) > 0 THEN true ELSE false END FROM Documento d WHERE d.path LIKE %:filename% AND (" +
+            "(EXISTS (SELECT 1 FROM Beneficio b WHERE b.termo = d AND b.ativo = true)) OR " +
+            "(EXISTS (SELECT 1 FROM Estudante e JOIN e.documentos ed WHERE ed = d AND e.ativo = true)))")
+    boolean isArquivoEssencialParaOSistema(@Param("filename") String filename);
 }
